4818e4d4517f380eb4d238cd2e5649e596f22c1d
Mai Uchida <mai.uchida1119@gmail.com>
2021-11-19 17:18:02 +0900

    Initial Commit

---
+++ TechNews(SwiftUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TechNews(SwiftUI)
+//
+//  Created by Mai Uchida on 2021/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TechNews(SwiftUI)/TechNews_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  TechNews_SwiftUI_App.swift
+//  TechNews(SwiftUI)
+//
+//  Created by Mai Uchida on 2021/11/19.
+//
+
+import SwiftUI
+
+@main
+struct TechNews_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
